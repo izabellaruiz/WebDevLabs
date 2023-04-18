2bfea523e42980d345e1ced75f3611c4ff8f6bd7
Bella <izabellaruiz@users.noreply.github.com>
2023-04-18 16:09:14 -0400

    lab 13 new button and header

--- Lab 13/js/button.js
+++ Lab 13/js/button.js
@@ -2,8 +2,10 @@
 function start() {
   //a variable that gets the button on line 11
   var button = document.getElementById('tChange');
+  var button2 = document.getElementById('hChange');
   //a command that says when button is clicked run the function 'change'
   button.onclick = change;
+  button2.onclick = changeHeader;
 }
 //a function that changes style elements
 function change(){
@@ -12,6 +14,11 @@ function change(){
   document.getElementById('change').style.font='times'
   document.getElementById('change').style.color='pink';
   document.getElementById('change').style.backgroundColor='black'
+
+}
+function changeHeader(){
+  document.getElementById('header').style.color='green';
+
 }
 //a command that says when the window loads, run the function 'start'
-    window.onload = start;
+  window.onload = start;
